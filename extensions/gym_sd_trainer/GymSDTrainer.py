967949fc05b5b1a71b1d0cbaf24696c0617bd5cd
Levi McCallum <levi@levimccallum.com>
2024-12-10 20:10:29 -0800

    feat: load save callback function whenever post hook is called

--- extensions/gym_sd_trainer/GymSDTrainer.py
+++ extensions/gym_sd_trainer/GymSDTrainer.py
@@ -11,22 +11,10 @@ class GymSDTrainer(SDTrainer):
         self, process_id: int, job: "ExtensionJob", config: OrderedDict, **kwargs
     ):
         super().__init__(process_id, job, config, **kwargs)
-        # Print full config for debugging
-        print("Full config:")
-        print(self.config)
-
-        # Get and debug the callback function from config
-        self.save_callback: Optional[Callable[[str], None]] = self.get_conf(
-            "save_callback"
-        )
-        print("Save callback value:", self.save_callback)
-        print("Save callback type:", type(self.save_callback))
 
     def post_save_hook(self, save_path: str):
         """Called after a model is saved"""
         super().post_save_hook(save_path)
-        print(f"Post save hook called for path: {save_path}")
-        if self.save_callback:
-            print("Executing save callback")
-            self.save_callback(save_path)
-            print("Save callback completed")
+        save_callback = self.get_conf("save_callback")
+        if save_callback:
+            save_callback(save_path)
